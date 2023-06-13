3db95308fc15aad15a95da4b2604eca8539d6ff5
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-13 17:26:55 +0800

    implementing about me section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,36 @@
-import Hero from '@/components/Hero'
-import React from 'react'
+import Image from 'next/image'
+import profile from '/public/assets/profile2.jpg'
 
 export default function page() {
   return (
     <div>
-      <Hero />
+      <div className='flex flex-col items-center mt-20'>
+        <Image
+          src={profile}
+          className='w-64 h-[450px]'
+        />
+        <div className='mt-10 w-80 flex flex-col gap-6 text-grey'>
+          <div>
+            <p>My name is Jefferson Balde. I’m a Front End Developer living in Philippines,
+            interested in how people and software can work together to make life better.
+            I specialize in <span className='text-blue'>responsive websites</span> and <span className='text-blue'>functional user interfaces.</span></p>
+          </div>
+          <div>
+            <p>I'm an irregular Computer Science student, passionate about programming 
+          and building responsive web and mobile applications. I transitioned from 
+          BSA to pursue my love for coding. Constantly seeking new knowledge 
+          and technologies.</p>
+          </div>
+          <div>
+            <p>I’m also a productivity nerd and love discovering new ways and skills. I 
+          enjoy reading/listening to books about growth and self-development, 
+          drinking coffee, patting my dogs, and going out for leisurely walks.</p>
+          </div>
+          <div>
+            <p className='text-2xl'>👨🏻‍💻&nbsp;&nbsp;📚&nbsp;&nbsp;☕&nbsp;&nbsp;🐕&nbsp;&nbsp;🚶🏻‍♂️</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className='mt-28 flex flex-col justify-center items-center text-center'>
         <h1 className='text-grey font-bold text-6xl sm:text-7xl lg:text-8xl'>Hi, I&apos;m Jeff.</h1>      
-        <h2 className='mt-7 text-[17px] text-grey w-80 sm:text-lg sm:w-[490px] md:text-xl md:w-[570px] lg:w-[710px]'>I’m a Freelance <span className='text-blue'>Front End Developer</span> who enjoys crafting high-performing 
+        <h2 className='mt-7 text-[17px] text-grey w-80 sm:text-lg sm:w-[490px] md:text-xl md:w-[570px] lg:w-[710px]'>I’m a Freelance <span className='text-grey'>Front End Developer</span> who enjoys crafting high-performing 
             beautiful websites that are conversion-focused, brand-accurate, 
             & people-friendly.
         </h2>
