30a2aa5f380f260e56bf06352c6ee3f0f5e17ccb
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-07-18 09:22:27 +0800

    adding works

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,7 @@ import project1 from '../public/assets/project1.png'
 import project2 from '../public/assets/project2.png'
 import project3 from '../public/assets/project3.png'
 import project4 from '../public/assets/project4.png'
+import project5 from '../public/assets/project5.png'
 import { motion, useAnimation, useInView } from 'framer-motion';
 import { idText } from 'typescript';
 
@@ -181,6 +182,34 @@ function Projects() {
                         <h6 className='text-grey text-[15px] md:text-[16px] ml-5'>HTML5 &nbsp;&nbsp; /&nbsp;&nbsp; CSS3 &nbsp;&nbsp; /&nbsp;&nbsp; JavaScript</h6>
                      </div>
          {/* </motion.div> */}
+        </div>
+
+        <div className='mt-12'>
+                <div className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'>
+                        <Image 
+                            src={project5}
+                            alt="scc website"
+                            className='object-cover group-hover:opacity-[0.02] h-64 w-[470px] bg-center sm:w-[623px] sm:h-[330px] md:w-[730px] md:h-[360px] lg:w-[850px] lg:h-[410px]'
+                        />
+                        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden group-hover:block w-[290px] sm:w-[500px] md:w-[610px] lg:w-[700px]'>
+                            <div>
+                                <p className='text-[18px] sm:text-xl md:text-2xl'> 
+                                    <span className='text-grey font-bold'>A school website for Saint Columban College </span> 
+                                    <span className='text-grey'>who enjoys crafting high-performing beautiful websites 
+                                    that are conversion-focused, brand-accurate, & people-friendly.</span>
+                                </p>
+                            </div>
+                            <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl'> 
+                                <a href="#" className='mr-4 '>View Website</a>
+                                <a href="#">Github</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='mt-1 flex flex-row items-center'>
+                        <h2 className='text-lg mr-5 text-black md:text-xl'>OptiReach</h2>
+                        {/* <h6 className='text-grey mr-5 text-[12px] md:text-[15px]'>/</h6> */}
+                        <h6 className='text-grey text-[15px] md:text-[16px] ml-5'>HTML5 &nbsp;&nbsp; /&nbsp;&nbsp; CSS3 &nbsp;&nbsp; /&nbsp;&nbsp; JavaScript</h6>
+                     </div>
         </div>
      </div>
   )
