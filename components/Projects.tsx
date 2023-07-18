e7bdd88eb4caba87fe776decfd8b39fa02f644b7
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-07-18 15:54:58 +0800

    updating title

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -77,26 +77,34 @@ function Projects() {
             animate={mainControls}
             transition={{duration: 0.5}}
         >
-            <div className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'>
-                    <Image 
-                        src={project2}
-                        alt="scc website"
-                        className='object-cover group-hover:opacity-[0.02] h-64 w-[470px] bg-center sm:w-[623px] sm:h-[330px] md:w-[730px] md:h-[360px] lg:w-[850px] lg:h-[410px]'
-                    />
-                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden group-hover:block w-[290px] sm:w-[500px] md:w-[610px] lg:w-[700px]'>
-                        <div>
-                            <p className='text-[18px] sm:text-xl md:text-2xl'> 
-                                <span className='text-grey font-bold'>A school website for Saint Columban College </span> 
-                                <span className='text-grey'>who enjoys crafting high-performing beautiful websites 
-                                that are conversion-focused, brand-accurate, & people-friendly.</span>
-                            </p>
-                        </div>
-                        <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl'> 
-                            <a href="#" className='mr-4 '>Live</a>
-                            <a href="#">Github</a>
-                        </div>
+            <motion.div 
+                className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'
+                whileHover={{
+                    x: 0,
+                    y: 0,
+                    scale: 0.99,
+                    rotate: 0,
+                }}
+                transition={{duration: 0.2}}
+            >
+                <Image 
+                    src={project2}
+                    alt="scc website"
+                    className='object-cover group-hover:opacity-[0.02] h-64 w-[470px] bg-center sm:w-[623px] sm:h-[330px] md:w-[730px] md:h-[360px] lg:w-[850px] lg:h-[410px]'
+                />
+                <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden group-hover:block w-[290px] sm:w-[500px] md:w-[610px] lg:w-[700px]'>
+                    <div className='text-center'>
+                        <p className='text-xl sm:text-3xl md:text-4xl'> 
+                            <span className='text-grey font-bold'>Revolve </span> 
+                            <span className='text-grey'>elevates your brand with creative geniuses, weaving magic through web design, marketing, and technology.</span>
+                        </p>
                     </div>
-            </div>
+                    <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl text-center'> 
+                        <a href="https://jrevolve.vercel.app/" target='_blank' className='mr-4 text-lg text-blue sm:text-2xl hover:underline decoration-1 '>View Website</a>
+                        {/* <a href="#">Github</a> */}
+                    </div>
+                </div>
+            </motion.div>
             <div className='mt-1 flex flex-row items-center'>
                 <h2 className='text-lg mr-5 text-black md:text-xl'>Revolve</h2>
                 {/* <h6 className='text-grey mr-5 text-[12px] md:text-[15px]'>/</h6> */}
@@ -115,31 +123,39 @@ function Projects() {
             animate={mainControls}
             transition={{duration: 0.5, delay: 0.5}}
         >
-                <div className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'>
+                <motion.div 
+                    className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'
+                    whileHover={{
+                        x: 0,
+                        y: 0,
+                        scale: 0.99,
+                        rotate: 0,
+                    }}
+                    transition={{duration: 0.2}}
+                >
                         <Image 
                             src={project5}
                             alt="scc website"
                             className='object-cover group-hover:opacity-[0.02] h-64 w-[470px] bg-center sm:w-[623px] sm:h-[330px] md:w-[730px] md:h-[360px] lg:w-[850px] lg:h-[410px]'
                         />
                         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden group-hover:block w-[290px] sm:w-[500px] md:w-[610px] lg:w-[700px]'>
-                            <div>
-                                <p className='text-[18px] sm:text-xl md:text-2xl'> 
-                                    <span className='text-grey font-bold'>A school website for Saint Columban College </span> 
-                                    <span className='text-grey'>who enjoys crafting high-performing beautiful websites 
-                                    that are conversion-focused, brand-accurate, & people-friendly.</span>
+                            <div className='text-center'>
+                                <p className='text-xl sm:text-3xl md:text-4xl'> 
+                                    <span className='text-grey font-bold'>OptiReach </span> 
+                                    <span className='text-grey'>elevates your brand with creative geniuses, weaving magic through web design, marketing, and technology.</span>
                                 </p>
                             </div>
-                            <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl'> 
-                                <a href="#" className='mr-4 '>Live</a>
-                                <a href="#">Github</a>
+                            <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl text-center'> 
+                                <a href="https://joptireach.vercel.app/" target='_blank' className='mr-4 text-lg text-blue sm:text-2xl hover:underline decoration-1 '>View Website</a>
+                                {/* <a href="#">Github</a> */}
                             </div>
                         </div>
-                    </div>
+                </motion.div>
                     <div className='mt-1 flex flex-row items-center'>
                         <h2 className='text-lg mr-5 text-black md:text-xl'>OptiReach</h2>
                         {/* <h6 className='text-grey mr-5 text-[12px] md:text-[15px]'>/</h6> */}
                         <h6 className='text-grey text-[15px] md:text-[16px] ml-5'>HTML5 &nbsp;&nbsp; /&nbsp;&nbsp; CSS3 &nbsp;&nbsp; /&nbsp;&nbsp; JavaScript</h6>
-                </div>
+                    </div>
                 
         </motion.div>
 
@@ -152,29 +168,37 @@ function Projects() {
             }}
             initial="hidden"
             animate={mainControls}
-            transition={{duration: 0.7, delay: 0.7}}
+            transition={{duration: 0.6, delay: 0.6}}
         > */}
         <div className='mt-12'>
-                <div className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'>
+                <motion.div 
+                    className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'
+                    whileHover={{
+                        x: 0,
+                        y: 0,
+                        scale: 0.99,
+                        rotate: 0,
+                    }}
+                    transition={{duration: 0.2}}
+                >
                         <Image 
                             src={project3}
                             alt="scc website"
                             className='object-cover group-hover:opacity-[0.02] h-64 w-[470px] bg-center sm:w-[623px] sm:h-[330px] md:w-[730px] md:h-[360px] lg:w-[850px] lg:h-[410px]'
                         />
                         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden group-hover:block w-[290px] sm:w-[500px] md:w-[610px] lg:w-[700px]'>
-                            <div>
-                                <p className='text-[18px] sm:text-xl md:text-2xl'> 
-                                    <span className='text-grey font-bold'>A school website for Saint Columban College </span> 
-                                    <span className='text-grey'>who enjoys crafting high-performing beautiful websites 
-                                    that are conversion-focused, brand-accurate, & people-friendly.</span>
+                            <div className='text-center'>
+                                <p className='text-xl sm:text-3xl md:text-4xl'> 
+                                    <span className='text-grey font-bold'>Fusion </span> 
+                                    <span className='text-grey'>elevates your brand with creative geniuses, weaving magic through web design, marketing, and technology.</span>
                                 </p>
                             </div>
-                            <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl'> 
-                                <a href="#" className='mr-4 '>Live</a>
-                                <a href="#">Github</a>
+                            <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl text-center'> 
+                                <a href="https://jfusion.vercel.app//" target='_blank' className='mr-4 text-lg text-blue sm:text-2xl hover:underline decoration-1 '>View Website</a>
+                                {/* <a href="#">Github</a> */}
                             </div>
                         </div>
-                    </div>
+                </motion.div>
                     <div className='mt-1 flex flex-row items-center'>
                         <h2 className='text-lg mr-5 text-black md:text-xl'>Fusion</h2>
                         {/* <h6 className='text-grey mr-5 text-[12px] md:text-[15px]'>/</h6> */}
@@ -184,26 +208,34 @@ function Projects() {
         </div>
 
         <div className='mt-12'>
-                <div className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'>
+                <motion.div 
+                    className='relative group hover:bg-[#FFFFFF] hover:shadow-slate-200 shadow-md'
+                    whileHover={{
+                        x: 0,
+                        y: 0,
+                        scale: 0.99,
+                        rotate: 0,
+                    }}
+                    transition={{duration: 0.2}}
+                >
                         <Image 
                             src={project4}
                             alt="scc website"
                             className='object-cover group-hover:opacity-[0.02] h-64 w-[470px] bg-center sm:w-[623px] sm:h-[330px] md:w-[730px] md:h-[360px] lg:w-[850px] lg:h-[410px]'
                         />
                         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden group-hover:block w-[290px] sm:w-[500px] md:w-[610px] lg:w-[700px]'>
-                            <div>
-                                <p className='text-[18px] sm:text-xl md:text-2xl'> 
-                                    <span className='text-grey font-bold'>A school website for Saint Columban College </span> 
-                                    <span className='text-grey'>who enjoys crafting high-performing beautiful websites 
-                                    that are conversion-focused, brand-accurate, & people-friendly.</span>
+                            <div className='text-center'>
+                                <p className='text-xl sm:text-3xl md:text-4xl'> 
+                                    <span className='text-grey font-bold'>MarketMinds </span> 
+                                    <span className='text-grey'>elevates your brand with creative geniuses, weaving magic through web design, marketing, and technology.</span>
                                 </p>
                             </div>
-                            <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl'> 
-                                <a href="#" className='mr-4 '>View Website</a>
-                                <a href="#">Github</a>
+                            <div className='mt-4 text-grey text-[17px] sm:text-xl md:text-2xl text-center'> 
+                                <a href="https://jmarketminds.vercel.app/" target='_blank' className='mr-4 text-lg text-blue sm:text-2xl hover:underline decoration-1 '>View Website</a>
+                                {/* <a href="#">Github</a> */}
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                     <div className='mt-1 flex flex-row items-center'>
                         <h2 className='text-lg mr-5 text-black md:text-xl'>MarketMinds</h2>
                         {/* <h6 className='text-grey mr-5 text-[12px] md:text-[15px]'>/</h6> */}
