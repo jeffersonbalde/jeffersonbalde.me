69434c449f8dfcca46c2a5404faa86569bb022c3
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-02-15 04:44:23 +0800

    test

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -372,7 +372,7 @@ function Projects() {
                         </div>
                 </motion.div>
                     <div className='mt-1 flex flex-row items-center'>
-                        <h2 className='text-lg mr-5 text-black md:text-xl'> POS and Inventory Management System </h2>
+                        <h2 className='text-lg mr-5 text-black md:text-xl'> POS and Inventory Management System  </h2>
                         {/* <h6 className='text-grey mr-5 text-[12px] md:text-[15px]'>/</h6> */}
                         <h6 className='text-grey text-[15px] md:text-[16px] ml-5'>C# &nbsp;&nbsp; /&nbsp;&nbsp; Microsoft SQL Server /&nbsp;&nbsp; Windows Form Application &nbsp;&nbsp; </h6>
                      </div>
